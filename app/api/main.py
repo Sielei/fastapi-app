fc8ae1bb2c38622ad06dd6d530ad582f63454786
Sielei Herman <hsielei@gmail.com>
2024-05-20 15:28:18 +0300

    add event, topic and group routes

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import users, auth
+from app.api.routes import users, auth, event, topic, group
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
+api_router.include_router(event.router, prefix="/events", tags=["Events"])
+api_router.include_router(topic.router, prefix="/topics", tags=["Topics"])
+api_router.include_router(group.router, prefix="/groups", tags=["Groups"])
